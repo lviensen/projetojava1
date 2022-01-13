c2e6abfaa28fe1e1d92b04d10ea7f32c943f2da4
vinic <vinic@DESKTOP-8JUVNTD>
2022-01-13 01:05:35 -0300

    alteração no texto apresentado no console

--- primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
+++ primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -3,6 +3,6 @@ package cursojava.executavel;
 public class PrimeiraClasseJava {
 
 	public static void main(String[] args) {
-		System.out.println("Meu primeiro projedo com o Eclipse 2022");
+		System.out.println("Alteração para commit");
 	}
 }
