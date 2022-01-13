4b6210a89a844094449ebb2d07192c4f73863994
vinic <vinic@DESKTOP-8JUVNTD>
2022-01-13 01:30:37 -0300

    adicionando o campo idade na classe pessoa

--- primeiro-programa-java/src/cursojava/vini/Pessoa.java
+++ primeiro-programa-java/src/cursojava/vini/Pessoa.java
@@ -2,4 +2,26 @@ package cursojava.vini;
 
 public class Pessoa {
 
+	private String nome;
+	private String sobrenome;
+	private int Idade;
+	
+	public int getIdade() {
+		return Idade;
+	}
+	public void setIdade(int idade) {
+		Idade = idade;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getSobrenome() {
+		return sobrenome;
+	}
+	public void setSobrenome(String sobrenome) {
+		this.sobrenome = sobrenome;
+	}
 }
