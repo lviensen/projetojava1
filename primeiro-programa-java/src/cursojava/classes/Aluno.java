f925ff0f430ddd4c02d2076d8e7ce3169a5340a1
vinic <vinic@DESKTOP-8JUVNTD>
2022-01-18 15:13:29 -0300

    Adicionada classe Aluno

---
+++ primeiro-programa-java/src/cursojava/classes/Aluno.java
@@ -0,0 +1,56 @@
+package cursojava.classes;
+
+public class Aluno {
+	
+	private String nome;
+	private int idade;
+	private String dataNasc;
+	private String numeroCpf;
+	private String matricula;
+	
+	public Aluno() {
+		
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+
+	public int getIdade() {
+		return idade;
+	}
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+
+	public String getDataNasc() {
+		return dataNasc;
+	}
+
+	public void setDataNasc(String dataNasc) {
+		this.dataNasc = dataNasc;
+	}
+
+	public String getNumeroCpf() {
+		return numeroCpf;
+	}
+
+	public void setNumeroCpf(String numeroCpf) {
+		this.numeroCpf = numeroCpf;
+	}
+
+	public String getMatricula() {
+		return matricula;
+	}
+
+	public void setMatricula(String matricula) {
+		this.matricula = matricula;
+	}
+	
+	
+}
--- primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
+++ primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -1,8 +1,17 @@
 package cursojava.executavel;
 
+import cursojava.classes.Aluno;
+
 public class PrimeiraClasseJava {
 
 	public static void main(String[] args) {
-		System.out.println("Alteração para commit");
+		Aluno aluno1 = new Aluno();
+		aluno1.setNome("Leandro");
+		aluno1.setIdade(26);
+		aluno1.setDataNasc("15/05/1995");
+		
+		Aluno aluno2 = new Aluno();
+		
+		System.out.println(aluno1.getNome());
 	}
 }
